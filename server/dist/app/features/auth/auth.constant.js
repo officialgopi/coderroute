61b195853e2d85444da5b4513b27f2fadd1be165
OfficialGopi <official.gopi@outlook.com>
2025-10-15 23:44:50 +0530

    dist  removed

--- server/dist/app/features/auth/auth.constant.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CORS_OPTIONS = void 0;
-const env_1 = require("../../../env");
-exports.CORS_OPTIONS = {
-    origin: env_1.env.CLIENT_URL,
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-};
