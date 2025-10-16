4634a2915224143dc4dcd91e3191f6aab5464c26
OfficialGopi <official.gopi@outlook.com>
2025-10-16 19:47:38 +0530

    feat: submission router added to Router Index

--- server/src/app/features/index.ts
+++ server/src/app/features/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { authRouter } from "./auth/auth.route";
 import { problemRouter } from "./problem/problem.route";
+import { submissionRouter } from "./submission/submission.route";
 
 const allRoutes = Router();
 
 allRoutes.use("/auth", authRouter);
 allRoutes.use("/problem", problemRouter);
+allRoutes.use("/submission", submissionRouter);
 
 export { allRoutes };
