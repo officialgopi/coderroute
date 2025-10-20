b3449858ee68195eb23fc69b11a40655a77734fe
OfficialGopi <official.gopi@outlook.com>
2025-10-20 16:47:02 +0530

    feat: discussion router added to routes in feature index

--- server/src/app/features/index.ts
+++ server/src/app/features/index.ts
@@ -5,6 +5,7 @@ import { submissionRouter } from "./submission/submission.route";
 import { codeExecutionRouter } from "./code-execution/code-execution.route";
 import { rateLimitting } from "../middlewares/rate-limitting.middleware";
 import { liveblocksRoutes } from "./collaborative-code-editor/collaborative-code-editor.route";
+import { discussionRouter } from "./discussion/discussion.route";
 
 const allRoutes = Router();
 
@@ -13,5 +14,6 @@ allRoutes.use("/problem", problemRouter);
 allRoutes.use("/submission", submissionRouter);
 allRoutes.use("/code-execution", rateLimitting(), codeExecutionRouter);
 allRoutes.use("/liveblocks", liveblocksRoutes);
+allRoutes.use("/discussion", discussionRouter);
 
 export { allRoutes };
