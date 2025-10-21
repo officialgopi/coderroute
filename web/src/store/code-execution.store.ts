128234912d40389d5bb4a51333cb1e78e60c2d6c
OfficialGopi <official.gopi@outlook.com>
2025-10-21 17:51:09 +0530

    feat: code execution route and controller added

--- web/src/store/code-execution.store.ts
+++ web/src/store/code-execution.store.ts
@@ -1,5 +1,7 @@
 import { create } from "zustand";
 import type { TLanguage } from "../types/types";
+import { apiCallHandler } from "../utils/api-call-handler.util";
+import { toast } from "sonner";
 interface CodeExecutionState {
   isRunning: boolean;
   isSubmitting: boolean;
@@ -29,19 +31,96 @@ interface CodeExecutionState {
     source_code: string,
     language: TLanguage
   ) => Promise<{
-    submission: {
-      id: string;
-      userId: string;
-      problemId: string;
-      sourceCode: string;
-      language: TLanguage;
-    };
+    submission:
+      | {
+          id: string;
+          userId: string;
+          problemId: string;
+          sourceCode: string;
+          language: TLanguage;
+          compileOutput?: string;
+          status: string;
+          memory?: string;
+          time?: string;
+          createdAt: string;
+          updatedAt: string;
+          testCasesResults: {
+            id: string;
+            submissionId: string;
+            testCaseId: number;
+            passed: boolean;
+            stdout?: string;
+            expected?: string;
+            stderr?: string;
+            compileOutput?: string;
+            status?: string;
+            memory?: string;
+            time?: string;
+            createdAt: Date;
+            updatedAt: Date;
+          }[];
+        }
+      | undefined;
   } | void>;
 }
 
 export const useCodeExecutionStore = create<CodeExecutionState>((set, get) => ({
   isRunning: false,
   isSubmitting: false,
-  runCode: async () => {},
-  submitCode: async () => {},
+  runCode: async (problemId, source_code, stdin, expected_output, langauge) => {
+    set({
+      isRunning: true,
+    });
+    try {
+      const response = await apiCallHandler(
+        `/code-execution/${problemId}/run`,
+        "POST",
+        {
+          source_code,
+          stdin,
+          expected_output,
+          langauge,
+        }
+      );
+      if (!response.data) {
+        toast.error("Failed to run code. Please try again.");
+        return;
+      }
+      return response.data;
+    } catch (error) {
+      toast.error("An error occurred while running the code.");
+    } finally {
+      if (get().isRunning)
+        set({
+          isRunning: false,
+        });
+    }
+  },
+  submitCode: async (problemId, source_code, language) => {
+    set({
+      isSubmitting: true,
+    });
+    try {
+      const response = await apiCallHandler(
+        `/code-execution/${problemId}/submit`,
+        "POST",
+        {
+          source_code,
+          language,
+        }
+      );
+      if (!response.data) {
+        toast.error("Failed to submit code. Please try again.");
+        return;
+      }
+      return response.data;
+    } catch (error) {
+      toast.error("An error occurred while submitting the code.");
+    } finally {
+      if (get().isSubmitting)
+        set({
+          isSubmitting: false,
+        });
+    }
+  },
 }));
