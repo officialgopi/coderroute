0f2c787954857771fcce3d04b8c679724f5432ab
OfficialGopi <official.gopi@outlook.com>
2025-11-05 16:47:23 +0530

    feat: Add ToastBar component for theme-based notifications

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,11 +1,18 @@
 import { createRoot } from "react-dom/client";
 import Router from "./router/Router.tsx";
 import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "./lib/theme.lib.tsx";
+import { ThemeProvider, useThemeStore } from "./lib/theme.lib.tsx";
+import { Toaster } from "sonner";
+
+const ToastBar = () => {
+  const { theme } = useThemeStore();
+  return <Toaster position="bottom-right" theme={theme} />;
+};
 
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider>
     <BrowserRouter>
+      <ToastBar />
       <Router />
     </BrowserRouter>
   </ThemeProvider>
