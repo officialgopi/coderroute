fab8d52d7f7838fb757bc4b480b80f272c2fef5a
OfficialGopi <official.gopi@outlook.com>
2025-11-13 21:52:58 +0530

    fix: Change PageLoader position from fixed to absolute for better layout control

--- web/src/components/loaders/PageLoader.tsx
+++ web/src/components/loaders/PageLoader.tsx
@@ -1,6 +1,6 @@
 const PageLoader = () => {
   return (
-    <div className="fixed top-[50%] left-[50%] animate-pulse transform -translate-x-1/2 -translate-y-1/2">
+    <div className="absolute top-[50%] left-[50%] animate-pulse transform -translate-x-1/2 -translate-y-1/2">
       <img src="/coderroute.png" alt="Loading..." className="  w-32 h-32 " />
       <div className="text-center mt-4 text-2xl ">
         <h2>Welcome to </h2>
