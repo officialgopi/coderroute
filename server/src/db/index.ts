96a6eefe6edbd54aa569fa3619dde0c18b47b74b
OfficialGopi <official.gopi@outlook.com>
2025-12-19 02:44:50 +0530

    fix: update package.json and package-lock.json to include @prisma/adapter-pg and pg dependencies; refactor db connection to use PrismaPg adapter

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,12 +1,32 @@
 import { PrismaClient } from "@prisma/client";
 import { env } from "../env";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
+const pool =
+  globalThis.__pgPool ??
+  new Pool({
+    connectionString: env.DATABASE_URL,
+    ssl: true,
+  });
 
-const prismaClient = globalThis.prisma ?? new PrismaClient();
+if (env.NODE_ENV !== "production") {
+  globalThis.__pgPool = pool;
+}
 
-if (env.NODE_ENV !== "production") globalThis.prisma = prismaClient;
+const adapter = new PrismaPg(pool);
 
-export { prismaClient as db };
+const prisma =
+  globalThis.prisma ??
+  new PrismaClient({
+    adapter,
+  });
+
+if (env.NODE_ENV !== "production") {
+  globalThis.prisma = prisma;
+}
+
+export { prisma as db };
