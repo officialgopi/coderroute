09358d5f07707ec3151a84ce7e905eb968f34073
OfficialGopi <official.gopi@outlook.com>
2026-04-30 17:51:53 +0530

    fix: update COOKIE_OPTIONS to set secure flag and change sameSite to none

--- server/src/app/features/auth/auth.constant.ts
+++ server/src/app/features/auth/auth.constant.ts
@@ -12,6 +12,7 @@ export const COOKIE_OPTIONS: CookieOptions = {
   // domain: env.CLIENT_URL,
   httpOnly: true,
   // secure: true,
-  sameSite: "lax",
+  secure: true,
+  sameSite: "none",
   maxAge: 7 * 24 * 60 * 60 * 1000,
 };
