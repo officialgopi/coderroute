0b101615324866fd0533eb887add36a81af141c6
OfficialGopi <official.gopi@outlook.com>
2026-04-30 17:39:04 +0530

    fix: add console log to display access token in isAuthenticated middleware

--- server/src/app/middlewares/auth.middleware.ts
+++ server/src/app/middlewares/auth.middleware.ts
@@ -6,7 +6,7 @@ import jwt from "jsonwebtoken";
 
 const isAuthenticated = AsyncHandler(async (req, res, next) => {
   const accessToken = req.cookies["access-token"];
-
+  console.log(accessToken);
   if (!accessToken) {
     throw new ApiError(401, "Unauthorized");
   }
